cc5ff95764628577cb7cb11e2e27b2b0c36ad1a5
fatjie <372761781@qq.com>
2020-09-02 17:41:02 +0800

    今日事今日毕

--- src/todo/header.vue
+++ src/todo/header.vue
@@ -1,15 +1,16 @@
 <template>
     <header class="main-header">
-        <h1>Todo</h1>
+        <h1>今日事，今日毕！</h1>
     </header>
 </template>
 
 <style lang="stylus" scoped>
 .main-header
+    margin-top:150px;
     text-align center
     h1
-        font-size 100px
-        color rgba(175,47,47,0.4)
+        font-size 65px
+        color rgba(175,47,47,0.8)
         font-weight 400
         margin 20px
 </style>
